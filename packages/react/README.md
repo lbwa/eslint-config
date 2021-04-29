28bad4f8727e0919ff4a2981100b758953b59d0b
Liu Bowen <mr_lbw@outlook.com>
2021-04-30 00:15:42 +0800

    feat(react): react tsx/jsx rules

---
+++ packages/react/README.md
@@ -0,0 +1,14 @@
+# @lbwa/eslint-config-react
+
+An opinionated eslint config based on [eslint-config-react-app](https://github.com/facebook/create-react-app/tree/master/packages/eslint-config-react-app).
+
+```js
+// .eslintrc.js
+module.exports = {
+  extends: ['@lbwa/eslint-config-react']
+}
+```
+
+## License
+
+MIT © [Liu Bowen](https://github.com/lbwa)
---
+++ packages/react/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['@lbwa/eslint-config-typescript', 'react-app', 'react-app/jest'],
+  // eslint default parser
+  // see https://eslint.org/docs/user-guide/configuring/plugins#specifying-parser
+  // and https://github.com/eslint/espree
+  parser: 'espree',
+  overrides: [
+    // see https://github.com/facebook/create-react-app/blob/v4.0.3/packages/eslint-config-react-app/index.js
+    Object.assign({}, require('eslint-config-react-app').overrides[0], {
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser'
+    })
+  ]
+}
