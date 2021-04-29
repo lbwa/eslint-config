a19d89c99bd96da9eec26585a31171837294b251
Liu Bowen <mr_lbw@outlook.com>
2021-04-30 00:22:35 +0800

    chore: why we redeclare typescript parser

--- packages/react/index.js
+++ packages/react/index.js
@@ -8,6 +8,7 @@ module.exports = {
     // see https://github.com/facebook/create-react-app/blob/v4.0.3/packages/eslint-config-react-app/index.js
     Object.assign({}, require('eslint-config-react-app').overrides[0], {
       files: ['**/*.ts?(x)'],
+      // redeclare typescript parser for ts files since we have declared parser options in root field
       parser: '@typescript-eslint/parser'
     })
   ]
